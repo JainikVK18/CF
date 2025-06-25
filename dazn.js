515f3c4d693c118142d8655b35810941ae8ceaa0
JainikVK18 <Jainikpatel2007@gmail.com>
2025-06-25 15:02:19 -0400

    Update dazn.js

--- dazn.js
+++ dazn.js
@@ -1,5 +1,5 @@
 const streamMap = {
-  m1: "https://dcb-fl-live.dtcdn.dazn.com/13j1a6agda1er1nxud7yaqxvfy/tv/stream.mpd?channel=1649&outlet=dazn-row&plang=en",
+  m1: "https://dca-co-live-gcr.gcdn.co/1c85w1ockfu6j1m71c82q664ng/tv/stream.mpd?channel=1659&outlet=dazn-row&plang=en",
   m2: "https://dcb-fl-live.dtcdn.dazn.com/r82kvk4ble7s1i0vi6hsh2qz3/tv/stream.mpd?channel=1648&outlet=dazn-row&plang=en",
   m3: "https://example.com/stream3.mpd",
   // Add more as needed
