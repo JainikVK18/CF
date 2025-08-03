834795f8b63faa4c0ddf052a8cd3bc73ccdbd513
JainikVK18 <Jainikpatel2007@gmail.com>
2025-08-03 14:28:33 -0400

    Update jainik.js

--- jainik.js
+++ jainik.js
@@ -7,7 +7,7 @@ const STREAMS = {
             "5332345367554fd39588283a2b949d02": "8abcd0b3126923b068c26508e24c9f69"
         }
     },
-    Bein1: {
+    bein1: {
         url: "https://linearjitp-playback.astro.com.my/dash-wv/linear/408/default_ott.mpd",
         keys: {
             "1a655189ab5c49eb235308c2b1a9e710": "3c4508af348844107f5e026a4fd6b16e"
