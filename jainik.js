681e8b8824b0c77bff926ff1c1ea27390cd61a73
JainikVK18 <Jainikpatel2007@gmail.com>
2025-10-19 05:51:33 -0400

    Update jainik.js

--- jainik.js
+++ jainik.js
@@ -2,9 +2,9 @@
 
 const STREAMS = {
     friendly: {
-        url: "https://tglmp04.akamaized.net/out/v1/5793335474d74566a8a0adfa260f22a9/manifest.mpd",
+        url: "https://live2.shoq.com.pk/live/eds/ICC_Women_World_Cup_2025/DASH/ICC_Women_World_Cup_2025.mpd",
         keys: {
-            "5332345367554fd39588283a2b949d02": "8abcd0b3126923b068c26508e24c9f69"
+            "a6dfc4f17d70d8d07639659193f03b25": "893eacfabe386144b81733d622a7ad17"
         }
     },
     t4: {
